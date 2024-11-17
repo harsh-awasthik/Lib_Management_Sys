07ef9e4553f05696d91845d70f1bc5610c305e3f
CodeQuestor-hub <pragatijain841@gmail.com>
2024-11-18 00:40:43 +0530

    applying regex in date format

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include <iostream>
+#include <iostream> 
+#include <regex> 
 using namespace std;
 #include "library.h"
 #include "functions.h"
+
+bool isValidDate(const string& date) {
+    // Regex pattern for DD-MM-YYYY format
+    regex datePattern("^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-[0-9]{4}$");
+    return regex_match(date, datePattern);
+}
 
 int main()
 {
     int choice;
     string date;
-    cout << "Enter date (DD-MM-YYYY) : ";
-    cin >> date; 
+    
+    while(true){
+        cout << "Enter date (DD-MM-YYYY) : ";
+        cin >> date;
+        if (isValidDate(date)) {
+            break; // Exit loop if date is valid
+        } else {
+            cout << "Invalid date format! Please enter the date in DD-MM-YYYY format.\n";
+        }
+    } 
 
     string options[] = {"Librarian", "Student"};
     int x = selection(options, 2);
